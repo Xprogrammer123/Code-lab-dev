d71600d78703518a522e995c038b1f6605500a68
Fawas saka <150188434+Code-lab-dev123@users.noreply.github.com>
2024-08-29 02:17:49 -0700

    first react app lauch NOW!!

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppRoutes from './Routes';
+import AppRoutes from './App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
